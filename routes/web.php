4e17c70fc77ce6443b720752e13f061e816f4a2c
nini <poikl15397@gmail.com>
2017-11-28 22:09:26 +0800

    設定 route

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/post', function () {
-    return view('post');
-});
+Route::get('posts', ['as' => 'posts.index', 'uses' => 'PostsController@index']);
+Route::get('post', ['as' => 'posts.show', 'uses' => 'PostsController@show']);
+Route::get('about', ['as' => 'posts.about', 'uses' => 'PostsController@about']);
+Route::get('contact', ['as' => 'posts.contact', 'uses' => 'PostsController@contact']);
