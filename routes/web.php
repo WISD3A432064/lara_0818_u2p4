c949d5a9efec17991c08a31e8a916e0891414897
nini <poikl15397@gmail.com>
2017-11-28 22:06:53 +0800

    設定 view 的主樣板、子樣板等結構

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
